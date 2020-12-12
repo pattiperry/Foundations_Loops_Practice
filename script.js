29212199290fd0bd54843bc9d03dfe918c921b8d
Patti Perry <pattiperry12@gmail.com>
2020-12-12 10:27:01 -0500

    loops practice and lightning exercises

---
+++ script.js
@@ -0,0 +1,71 @@
+// LOOPS
+
+let moviesArray = ["It", "Et", "Up", "It 2", "D2", "Us", "Red", "Go"]
+
+
+// While Loops
+
+// console.log(moviesArray.length)
+
+// let i = 0
+
+// while(i < moviesArray.length){
+//     console.log(moviesArray[i])
+//     console.log(i)
+//     i++
+// }
+
+
+
+
+// Lightning Exercise
+// Psychotic Bird
+// In your JavaScript file, write a while loop that outputs the phrase "TWEET TWEET" 100 times to the console.
+let i = 0;
+
+while(i < 100){
+    console.log("TWEET TWEET")
+    i++
+}
+
+
+// let i = 1
+
+// while(i < 20){
+//     console.log("Hello World!")
+//     console.log(i)
+//     i++
+// }
+
+
+// For Loops
+
+
+for(i = 0; i < moviesArray.length; i++){
+    console.log(moviesArray[i])
+}
+
+
+// Favorite Foods Lightning Exercise
+// In your JavaScript, create a new variable called favoriteFoods. It should store an array of strings representing five of your favorite foods.
+// Loop through the array and print each food item to the console.
+
+let favoriteFoods = ["pizza", "burger", "fries", "hot dogs", "salad"]
+
+for(i = 0; i < favoriteFoods.length; i++){
+    console.log(favoriteFoods[i])
+}
+
+
+
+
+
+
+// Loops Practice 
+
+let cohortOneStudents = [Sable, David, Joey, Nick, Tommy, Connor, Charles, Russ, Bobby, Josh, Nikki, Matt, Dale, Sydney]
+
+for(i = 0; i < cohortOneStudents.length; i++){
+    console.log(cohortOneStudents[i])
+}
+
